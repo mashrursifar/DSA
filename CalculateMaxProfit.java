d26824470bea688ef5b9743686f241ad12bd897f
mashrursifar <mashrursifar@gmail.com>
2026-03-02 01:22:24 +0600

    Profit cal done

---
+++ CalculateMaxProfit.java
@@ -0,0 +1,26 @@
+public class CalculateMaxProfit {
+    public static int maxProfit(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]>buyPrice)
+            {
+                int profit = prices[i] -buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7,1,3,4,6};
+
+        System.out.println(maxProfit(prices));
+    }
+    
+}
