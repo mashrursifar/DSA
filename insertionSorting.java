a8eb5ed52965417844f2b2cf7d39124a3561c75c
mashrursifar <mashrursifar@gmail.com>
2026-03-02 23:17:53 +0600

    finished

---
+++ insertionSorting.java
@@ -0,0 +1,13 @@
+public class insertionSorting {
+
+
+    public static void main(String[] args) {
+        int arr[] = {5,3,7,3,1,2};
+
+        insertionSort(arr);
+
+        for(int i=0;i<arr.length;i++)
+            System.out.println(arr[i]+" ");
+    }
+    
+}
