f54f2e8fbd89a2ea76efcef793de5b065aa39e74
mashrursifar <mashrursifar@gmail.com>
2026-03-03 21:11:02 +0600

    Insertion Sorting Algo

--- insertionSorting.java
+++ insertionSorting.java
@@ -1,5 +1,22 @@
 public class insertionSorting {
 
+    public static void insertionSort(int arr[])
+    {
+       for(int i=1;i<arr.length;i++) 
+       {
+        int curr = arr[i];
+        int prev = i-1;
+        // FInding out the correct position to insert
+        while (prev>=0 && arr[prev] > curr) {
+            arr[prev+1] = arr[prev];
+            prev--;
+            
+        }
+        // Inertion
+        arr[prev+1] = curr;
+        
+       }
+    }
 
     public static void main(String[] args) {
         int arr[] = {5,3,7,3,1,2};
