3cc2bab8a6ed6000909d085b3d00b1ea676da05d
mashrursifar <mashrursifar@gmail.com>
2026-03-05 22:25:45 +0600

    2D array max min finder

--- twoDArray.java
+++ twoDArray.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 public class twoDArray {
 
+    public static void maxMin(int matrix[][])
+    {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for(int i =0;i<matrix.length; i++)
+        {
+            for(int j=0; j<matrix[0].length; j++)
+            {
+                max = max < matrix[i][j]? matrix[i][j]: max;
+                min = min > matrix[i][j]? matrix[i][j]: min;
+                
+            }
+        }
+        System.out.println("Maximum value: "+max);
+        System.out.println("Minimum value: "+min);
+    }
+
     public static void main(String[] args) {
         
         int matrix[][] = new int[3][3];
@@ -26,6 +42,7 @@ public class twoDArray {
             System.out.println();
         }
 
+        maxMin(matrix);
 
 
 
