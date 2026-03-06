e81b142553b1e8d3239c01703fc32d064ca2776d
mashrursifar <mashrursifar@gmail.com>
2026-03-06 22:17:54 +0600

    spiralArray done

--- spiralArray.java
+++ spiralArray.java
@@ -1,27 +1,59 @@
 public class spiralArray {
+    public static void arrPrint(int matrix[][]){
+        System.out.println("");
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0; j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    
     public static void spiralPrint(int matrix[][]){
 
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int rowEnd = matrix.length-1;
+        int colEnd = matrix[0].length-1;
+        int rowStart = 0, colStart = 0;
 
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-            System.out.print(matrix[i][j]+" ");
+
+        while(rowStart<=rowEnd && colStart<=colEnd)
+        {
+            // Top
+            for(int i = colStart;i<=colEnd;i++){
+                System.out.print(matrix[rowStart][i]+" ");
+            }
+
+            // Right
+            for(int i= rowStart+1; i<=rowEnd; i++){
+                System.out.print(matrix[i][colEnd]+" ");
+            }
+
+            // Bottom
+            for(int i= colEnd-1; i>=colStart; i--){
+                System.out.print(matrix[rowEnd][i]+" ");
+            }
+
+            // Left
+            for(int i= rowEnd-1;i>rowStart;i--){
+                System.out.print(matrix[i][colStart]+" ");
+            }
+
+            rowStart++;
+            rowEnd--;
+            colStart++;
+            colEnd--;
         }
-        }
+        System.out.println();
 
 
     }
     public static void main(String[] args) {
-        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int matrix[][] = {{1,2,3,4,17},{5,6,7,8,18},{9,10,11,12,19},{13,14,15,16,20},{21,22,23,24,25}};
 
         spiralPrint(matrix);
-        // for(int i=0;i<matrix.length;i++){
-        //     for(int j=0; j<matrix[0].length;j++){
-        //         System.out.print(matrix[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
+        
+        arrPrint(matrix);
     
     }
 }
