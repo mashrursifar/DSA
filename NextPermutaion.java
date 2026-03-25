d4a6cd0758b8e3049925a2b39de1cf6431abb3d7
mashrursifar <mashrursifar@gmail.com>
2026-03-25 23:07:36 +0600

    pr starting

---
+++ NextPermutaion.java
@@ -0,0 +1,16 @@
+public class NextPermutaion {
+    
+        public static int[] next(int arr[]){
+
+
+
+            return arr;
+        }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+
+        next(arr);
+    }
+}
