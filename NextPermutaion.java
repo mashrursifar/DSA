97fa482e8499da8a802083cb6cd5b6c80c8d39fb
mashrursifar <mashrursifar@gmail.com>
2026-03-26 11:53:34 +0600

    checking

--- NextPermutaion.java
+++ NextPermutaion.java
@@ -1,33 +1,56 @@
+import java.util.Arrays;
+
 public class NextPermutaion {
     
         public static int[] next(int arr[]){
             int n = arr.length-1;
             int i=n;
             int pivot = n-1;
+            boolean con = true;
 
             while (i>pivot && pivot>=0 && i>=0) {
                 
                 if(arr[pivot]<arr[i]){
-                    pivot--;
+                    con = false;
+
+                    // if(arr[i]>arr[n]){
+                    //     n--;
+                    //     continue;
+                    // }
+
                     int temp = arr[pivot];
                     arr[pivot] = arr[n];
                     arr[n] = temp;
+                    
+                    while (i<=n) {
+                        
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                        i++;
+                        n--;
+                    }
+                    break;
                 }else{
                     pivot--;
                     i--;
                 }
+                
+                
             }
-
+            if(con == true){
+                    Arrays.sort(arr);
+                }
             return arr;
         }
 
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4};
+        int arr[] = {1,4,3,2};
 
         next(arr);
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 }
