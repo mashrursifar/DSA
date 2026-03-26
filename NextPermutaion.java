2a94760bd3b7ccb8080070e8d1533afd0cb3b81c
mashrursifar <mashrursifar@gmail.com>
2026-03-26 13:28:18 +0600

    solved

--- NextPermutaion.java
+++ NextPermutaion.java
@@ -2,55 +2,59 @@ import java.util.Arrays;
 
 public class NextPermutaion {
     
-        public static int[] next(int arr[]){
+        public static void next(int arr[]){
             int n = arr.length-1;
             int i=n;
-            int pivot = n-1;
-            boolean con = true;
+            int pivot = -1;
 
-            while (i>pivot && pivot>=0 && i>=0) {
-                
-                if(arr[pivot]<arr[i]){
-                    con = false;
-
-                    // if(arr[i]>arr[n]){
-                    //     n--;
-                    //     continue;
-                    // }
-
-                    int temp = arr[pivot];
-                    arr[pivot] = arr[n];
-                    arr[n] = temp;
-                    
-                    while (i<=n) {
-                        
-                        temp = arr[i];
-                        arr[i] = arr[n];
-                        arr[n] = temp;
-                        i++;
-                        n--;
-                    }
+            while (i>0) {
+                if(arr[i]>arr[i-1]){
+                    pivot = i-1;
                     break;
-                }else{
-                    pivot--;
-                    i--;
                 }
-                
-                
+                i--;
             }
-            if(con == true){
-                    Arrays.sort(arr);
+            System.out.println(pivot);
+            
+
+            if(pivot==-1){
+                Arrays.sort(arr);
+                return;
+            }
+
+            i=n;
+            
+            while (i>=pivot) {
+                if (arr[pivot]<arr[i]) {
+                    int temp = arr[pivot];
+                    arr[pivot] = arr[i];
+                    arr[i] = temp;
+                    break;
                 }
-            return arr;
+                i--;
+            }
+
+
+            pivot++;
+            while (n>pivot) {
+                int temp = arr[pivot];
+                arr[pivot] = arr[n];
+                arr[n] = temp;
+                n--;
+                pivot++;
+            }
+
+            
         }
 
 
     public static void main(String[] args) {
-        int arr[] = {1,4,3,2};
+        int arr[] = {1,3,2};
 
         next(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
+            
         }
     }
 }
