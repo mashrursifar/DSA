8f3e484920d8d7fe2e62214e9779a7ecc1256ca5
mashrursifar <mashrursifar@gmail.com>
2026-04-10 18:03:19 +0600

    Recursion

--- Recursion/PrintNto1.java
+++ Recursion/PrintNto1.java
@@ -2,8 +2,18 @@ package Recursion;
 
 public class PrintNto1 {
     
-
-    public static void main(String[] args) {
+    public static void print(int n){
         
+
+        if (n==1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        print(10);
     }
 }
