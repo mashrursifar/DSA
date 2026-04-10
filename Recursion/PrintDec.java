ca614f414b3612d2974903c371164b722f65becd
mashrursifar <mashrursifar@gmail.com>
2026-04-10 18:07:24 +0600

    Recursion inc and dec

---
+++ Recursion/PrintDec.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+public class PrintDec {
+    
+    public static void printDec(int n){
+        
+
+        if (n==1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+
+        printDec(n-1);
+    }
+
+
+    public static void printInc(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        printInc(10);
+    }
+}
