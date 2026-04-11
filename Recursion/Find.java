a49bc9848f90b0df9879617191841929f9a3c8af
mashrursifar <mashrursifar@gmail.com>
2026-04-11 10:18:49 +0600

    LAst done

--- Recursion/Find.java
+++ Recursion/Find.java
@@ -14,9 +14,23 @@ public class Find {
         return firstOccurance(arr, key,i+1);
     }
 
+    public static int lastOccurance(int arr[], int key, int i){
+
+        if(i == arr.length){
+            return -1;
+        }
+
+        int isFound = lastOccurance(arr, key, i+1);
+
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+
+        return isFound;
+    }
     public static void main(String[] args) {
-        int arr[] = {1,4,3,3,2,4,5,3};
-        System.out.println(firstOccurance(arr, 9, 0));
+        int arr[] = {1,4,3,3,2,4,5,33,4,2,1};
+        System.out.println(lastOccurance(arr, 3, 0));
     }
     
 }
