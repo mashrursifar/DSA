cf7e936317c0d97c020bf3cc35a98b4de3ae626a
mashrursifar <mashrursifar@gmail.com>
2026-04-13 00:00:35 +0600

    puts

--- Recursion/tileFilling.java
+++ Recursion/tileFilling.java
@@ -1,3 +1,21 @@
+package Recursion; 
+
 public class tileFilling {
+    public static int tileNumber(int n){
+
+        if(n==0 || n ==1){
+            return 1;
+        }
+
+        int verticalNumber = tileNumber(n-1);
+        int horizontalNumber = tileNumber(n-2);
+        int totalways = verticalNumber + horizontalNumber;
+
+        return totalways;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tileNumber(3));
+    }
     
 }
