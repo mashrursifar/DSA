a1105b501d2508074525d46c0ace48ab070b8714
mashrursifar <mashrursifar@gmail.com>
2026-05-05 20:32:20 +0600

    Progressing

--- Devide/MergeSort.java
+++ Devide/MergeSort.java
@@ -3,17 +3,58 @@ package Devide;
 public class MergeSort {
 
 
-    public static void merge(int arr[],int si,int ei){
+    public static void divide(int arr[],int si,int ei){
 
         if(si>=ei){
             return;
         }
 
         int mid = si + (ei - si)/2;
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
 
+        merge(arr, si, ei, mid);
 
 
         return;
+    }
+
+    public static void merge(int arr[], int si,int ei, int mid){
+        int temp[] = new int[ei-si+1];
+
+        int i = si;
+        int j = mid+1;
+        int k =0; 
+
+        while (i<=mid && j<= ei) {
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
+                i++;
+                j++;
+            }else{
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i<=mid) {
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+
+        while (j<=ei) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(k=0, i=si;k<temp.length;k++,i++){
+
+            arr[i] = temp[k];
+
+        }
     }
 
 
@@ -21,12 +62,15 @@ public class MergeSort {
     public static void printArr(int arr[])
     {for(int i=0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
-    }}
+    }
+System.out.println();}
 
     public static void main(String[] args) {
         int arr[] = {6,3,9,5,2,8};
 
-
+        printArr(arr);
+        
+        divide(arr, 0, arr.length-1);
         printArr(arr);
     }
     
