89d4fab70b03df7a277eb804059aa75e16e8eee4
mashrursifar <mashrursifar@gmail.com>
2026-05-05 22:02:38 +0600

    quic stated

---
+++ Devide/QuickSort.java
@@ -0,0 +1,21 @@
+package Devide;
+
+public class QuickSort {
+
+    public static void quickSort(int arr[],int si, int ei){
+        if()
+    }
+
+    public static void printArr(int arr[])
+    {for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,8,8,2,5};
+
+        quickSort(arr,0,arr.length);
+        printArr(arr);
+    }
+    
+}
