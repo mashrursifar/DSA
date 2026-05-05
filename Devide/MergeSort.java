9fb16fe73f55e0b76fc2f4c660d0b9d28784c3fc
mashrursifar <mashrursifar@gmail.com>
2026-05-05 20:16:42 +0600

    pust

---
+++ Devide/MergeSort.java
@@ -0,0 +1,33 @@
+package Devide;
+
+public class MergeSort {
+
+
+    public static void merge(int arr[],int si,int ei){
+
+        if(si>=ei){
+            return;
+        }
+
+        int mid = si + (ei - si)/2;
+
+
+
+        return;
+    }
+
+
+
+    public static void printArr(int arr[])
+    {for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }}
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,5,2,8};
+
+
+        printArr(arr);
+    }
+    
+}
