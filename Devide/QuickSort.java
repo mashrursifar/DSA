bed491a95b1ab0451ed728a63c26a3b20d22dd33
mashrursifar <mashrursifar@gmail.com>
2026-05-06 20:00:00 +0600

    Quick Sort Finished

--- Devide/QuickSort.java
+++ Devide/QuickSort.java
@@ -3,18 +3,51 @@ package Devide;
 public class QuickSort {
 
     public static void quickSort(int arr[],int si, int ei){
-        if()
+        if(si>=ei){
+            return;
+        }
+
+        int pIdx = partition(arr,si,ei);
+        quickSort(arr, si, pIdx-1); //left side of the pivot
+        quickSort(arr, pIdx+1, ei); //right side of the pivot
+    }
+
+    public static int partition(int arr[], int si, int ei){
+
+        int pivot = arr[ei];
+
+        int i = si -1;
+
+        for(int j=si; j<ei; j++){                     
+        
+            if(arr[j]<pivot){
+                i++;
+                
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+ 
+            }
+        }
+
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
     }
 
     public static void printArr(int arr[])
     {for(int i=0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
+        
+    }System.out.println();
     }
-
     public static void main(String[] args) {
         int arr[] = {6,3,8,8,2,5};
 
-        quickSort(arr,0,arr.length);
+        quickSort(arr,0,arr.length-1);
         printArr(arr);
     }
     
