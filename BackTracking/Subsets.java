1f6f16b2511148c838229594556158a83cd5a746
mashrursifar <mashrursifar@gmail.com>
2026-05-08 00:04:04 +0600

    subsets done

--- BackTracking/Subsets.java
+++ BackTracking/Subsets.java
@@ -3,7 +3,19 @@ package BackTracking;
 public class Subsets {
     public static void subset(String str, String ans, int i){
 
-        if()
+        if(i == str.length()){
+            System.out.println(ans);
+            return;
+        }
+
+        subset(str, ans+str.charAt(i), i+1);
+        subset(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        subset(str, "", 0);
     }
     
 }
