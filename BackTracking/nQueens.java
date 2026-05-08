8f944dd73db6a04f5c8a41057719e77f84f7dcb8
mashrursifar <mashrursifar@gmail.com>
2026-05-08 22:10:19 +0600

    Gridddd

--- BackTracking/nQueens.java
+++ BackTracking/nQueens.java
@@ -56,7 +56,7 @@ public class nQueens {
         return true;
     }
     public static void main(String[] args) {
-        int n=4;
+        int n=5;
         char chess[][] = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
