6c524d367bb5eb65ba44ca511c4813b0f6ca1a44
mashrursifar <mashrursifar@gmail.com>
2026-05-08 19:48:15 +0600

    Understood

--- BackTracking/Permutation.java
+++ BackTracking/Permutation.java
@@ -4,14 +4,17 @@ public class Permutation {
 
     public static void findPermudation(String str,String ans){
 
-        if(str.length() ==0){
+        if(str.length() == 0){
             System.out.println(ans);
             return;
         }
-
+        
     for(int i=0;i<str.length();i++){
+
         char curr = str.charAt(i);
         String newStr = str.substring(0,i) + str.substring(i+1);
+
+        // System.out.println("For loop i= "+i+" curr="+curr+" newStr= "+newStr+" str= "+str+" Ans= "+ans+curr);
 
         findPermudation(newStr, ans+curr);
     }
