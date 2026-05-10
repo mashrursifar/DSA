11e279aefc3a47a5837e8bcdb7ac5a8da1afaecd
mashrursifar <mashrursifar@gmail.com>
2026-05-10 22:09:46 +0600

    swapped

--- ArrayList/Test.java
+++ ArrayList/Test.java
@@ -3,6 +3,13 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class Test {
+
+    public static void swap(ArrayList<Integer> list,int idx1,int idx2){
+
+        Integer temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String[] args) {
         
         ArrayList<Integer> list = new ArrayList<>();
@@ -10,6 +17,16 @@ public class Test {
         list.add(1);
         list.add(3);
         list.add(5);
+        list.add(7);
+        list.add(9);
+
+
+        System.out.println(list);
+
+        list.set(3, 199);
+        System.out.println(list);
+
+        swap(list,1,3);
         System.out.println(list);
     }
     
