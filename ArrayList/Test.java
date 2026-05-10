914d2cfe18472be998f9be14a1e0912096233d54
mashrursifar <mashrursifar@gmail.com>
2026-05-10 21:56:15 +0600

    ArrayList

---
+++ ArrayList/Test.java
@@ -0,0 +1,16 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        System.out.println(list);
+    }
+    
+}
