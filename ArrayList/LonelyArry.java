d6a551af044c8d1492098179cf5c2155bd353b72
mashrursifar <mashrursifar@gmail.com>
2026-05-13 20:53:37 +0600

    Mid way

--- ArrayList/LonelyArry.java
+++ ArrayList/LonelyArry.java
@@ -6,7 +6,26 @@ public class LonelyArry {
     public static List<Integer> lonely(int nums[]){
         List<Integer> list = new ArrayList<>();
 
-        
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(i==n-1){
+                if(nums[i]!=nums[i-1] && nums[i] != nums[i-1]+1){
+                    list.add(nums[i]);
+                }
+                continue;
+            }
+            if(i==0){
+                if(nums[i]!=nums[i+1] && nums[i] != nums[i+1]-1){
+                    list.add(nums[i]);
+                }
+                continue;
+            }
+
+            
+
+
+        }
 
         return list;
     }
