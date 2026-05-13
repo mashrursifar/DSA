7b31f7fb0b11ec5720b106886082c027a1aa73e9
mashrursifar <mashrursifar@gmail.com>
2026-05-13 22:09:13 +0600

    LonelyArry done

--- ArrayList/LonelyArry.java
+++ ArrayList/LonelyArry.java
@@ -8,7 +8,12 @@ public class LonelyArry {
 
         Arrays.sort(nums);
         int n = nums.length;
+
+        if(n==1){
+            list.add(nums[0]);
+        }
         for(int i=0; i<n; i++){
+
             if(i==n-1){
                 if(nums[i]!=nums[i-1] && nums[i] != nums[i-1]+1){
                     list.add(nums[i]);
@@ -22,8 +27,12 @@ public class LonelyArry {
                 continue;
             }
 
-            
+            if(nums[i]!=nums[i+1] && nums[i] != nums[i+1]-1 && nums[i]!=nums[i-1] && nums[i] != nums[i-1]+1){
+                list.add(nums[i]);
+                
+            }
 
+            
 
         }
 
