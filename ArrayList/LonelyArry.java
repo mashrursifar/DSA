e1f42173c9447c5678b47a375c18882bbb70105f
mashrursifar <mashrursifar@gmail.com>
2026-05-13 20:25:08 +0600

    LonelyArry brainsroming

---
+++ ArrayList/LonelyArry.java
@@ -0,0 +1,18 @@
+package ArrayList;
+import java.util.*;
+
+public class LonelyArry {
+
+    public static List<Integer> lonely(int nums[]){
+        List<Integer> list = new ArrayList<>();
+
+        
+
+        return list;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,3,5,3};
+
+        System.out.println(lonely(nums));
+    }
+}
