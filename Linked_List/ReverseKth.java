c75c26a0c4f044ed1212fcefb89a2e1567bbd650
mashrursifar <mashrursifar@gmail.com>
2026-05-17 22:12:39 +0600

    linked LIst problem

---
+++ Linked_List/ReverseKth.java
@@ -0,0 +1,17 @@
+package Linked_List;
+
+public class ReverseKth {
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+
+        ll.print();
+    }
+}
