bbd017e7d5d9e8813161f857612ab8bff7066409
mashrursifar <mashrursifar@gmail.com>
2026-05-24 16:31:40 +0600

    3 sum

---
+++ ThreeSum.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static  List<List<Integer>> threeSum(int arr[]){
+        List<List<Integer>> list = new ArrayList<>();
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1; j<n-1; j++){
+                // Append
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+       
+        int arr[] = {-1,0,1,2,-1,-4};
+
+        threeSum(arr);
+    }
+}
