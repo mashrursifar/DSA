f0457b1c2e117ffc2d909e93e6e29ede21b403f2
mashrursifar <mashrursifar@gmail.com>
2026-05-26 12:33:31 +0600

    Solved next greater 2

--- Stack_/next2.java
+++ Stack_/next2.java
@@ -3,12 +3,22 @@ package Stack_;
 import java.util.Stack;
 
 public class next2 {
-    
-    public static void solution(int arr[], int nextG[]){
-        int n = arr.length;
-        int nextL[] = new int[n];
-        Stack<Integer> s = new Stack<>();
+    public static void checkLeft(int arr[], int nextG[], int i){
+        for(int j=0;j<i; j++){
+            if (arr[j] > arr[i] ) {
+                nextG[i] = arr[j];
+                return;
+            }
+        }
 
+    }
+    public static void solution(int arr[], int nextG[]){
+        int n = arr.length, max = Integer.MIN_VALUE;
+        Stack<Integer> s = new Stack<>();
+        for(int i=0; i<n; i++){
+            max = max>arr[i]?max:arr[i];
+        }
+        
         for(int i =n-1; i>=0; i--){
 
             while (!s.isEmpty()) {
@@ -21,6 +31,10 @@ public class next2 {
 
             if (s.isEmpty()) {
                 nextG[i] = -1;
+                if (arr[i]<max) {
+                    checkLeft(arr,nextG,i);
+                }
+
             }else{
                 nextG[i] = arr[s.peek()];
             }
@@ -28,7 +42,7 @@ public class next2 {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {1,3,4,2};
+        int arr[] = {1,2,6,1,5,4,3,1};
         int nextG[] = new int[arr.length];
 
         solution(arr,nextG);
