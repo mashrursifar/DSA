d693690c49d28d08dc5c011b5a20bd364602968e
mashrursifar <mashrursifar@gmail.com>
2026-05-27 14:06:48 +0600

    on the way

--- Stack_/DuplicateParentheses.java
+++ Stack_/DuplicateParentheses.java
@@ -7,36 +7,36 @@ public class DuplicateParentheses {
     public static boolean isDuplicate(String str){
         Stack<Character> s = new Stack<>();
         char ch = str.charAt(0);
+        int n = str.length();
+        int count =-1;
 
-        if (ch == '(' || ch == '[' || ch == '{') {
-            for(int i =1; i< str.length(); i++){
+        if (ch == '(') {
+            s.push(ch);
 
+            for(int i=1; i<n; i++){
+                ch = str.charAt(i);
                 s.push(ch);
-                
-
-                if (ch == ')' || ch == '}' || ch == ']') {
-                    while (!s.isEmpty() && !(ch == '(' || ch == '[' || ch == '{')) {
-                        System.out.println(s.peek()+" Pop");
+                if (ch == ')') {
+                    char top = s.peek();
+                    while (!s.isEmpty() && !(top == '(')) {
                         s.pop();
-                        ch = s.peek();
-
+                        top = s.peek();
+                        count++;
                     }
                     s.pop();
-                    System.out.println(s.peek()+" peek now after pop");
+                    System.out.println(count+" count");
+                    if(count<1) return true;
                 }
-                ch = str.charAt(i);
+                count = -1;
+                
             }
-            
-            
         }
-        System.out.println(s.peek()+" last");
-        if (s.isEmpty()) {
-            return true;
-        }
+        
         return false;
+        
     }
     public static void main(String[] args) {
-        String str = "((a+b)+(b-c))";
+        String str = "((a+b)+((a-b)))";
 
         System.out.println(isDuplicate(str));
     }
