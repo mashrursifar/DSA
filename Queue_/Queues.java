b5289a58386004c0b7f859803c635da63d118f7e
mashrursifar <mashrursifar@gmail.com>
2026-06-09 20:33:22 +0600

    queue done

--- Queue_/Queues.java
+++ Queue_/Queues.java
@@ -24,6 +24,43 @@ public class Queues {
             rear++;
             arr[rear] = data;
         }
+
+        public static  int remove(){
+            if(rear == -1){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+
+            int front = arr[0];
+            for(int i=0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+        public static int peek(){
+            if(rear == -1){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            return front;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.peek()+" ");
+            q.remove();
+        }
     }
 
     
