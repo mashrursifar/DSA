dc7aa03bb9521e3cdc96652c2ad95b4133b34546
mashrursifar <mashrursifar@gmail.com>
2026-06-09 20:25:46 +0600

    Queue started

---
+++ Queue_/Queues.java
@@ -0,0 +1,30 @@
+package Queue_;
+
+public class Queues {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+    
+        public static void add(int data){
+            if (rear == size-1) {
+                System.out.println("Queue is full");
+                return;
+            }
+
+            rear++;
+            arr[rear] = data;
+        }
+    }
+
+    
+}
