016222d4e7551b508af55f1611b43abe4374b871
mashrursifar <mashrursifar@gmail.com>
2026-06-10 16:45:53 +0600

    Finding first non Repeting  Character added

---
+++ Queue_/FirstNonRepetingChar.java
@@ -0,0 +1,38 @@
+package Queue_;
+
+import java.util.*;
+
+public class FirstNonRepetingChar {
+    
+    public static void findNonRepeting(String str){
+        Queue<Character> q = new LinkedList<>();
+        int freq[] = new int[26];
+        String s = "";
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            freq[ch-'a']++;
+            q.add(ch);
+
+            while (!q.isEmpty()) {
+                if (freq[q.peek()-'a']==1) {
+                    s += " "+q.peek();
+                    break;
+                }else{
+                    q.remove();
+                }
+            }
+            if (q.isEmpty()) {
+                s += " -1";   
+            }
+
+
+        }
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        String str = "aabccxb";
+
+        findNonRepeting(str);
+    }
+}
