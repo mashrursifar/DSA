ca580f371af9675b5359107f90cf0f176dc11632
mashrursifar <mashrursifar@gmail.com>
2026-07-07 22:15:26 +0600

    JS Started

---
+++ Developments/Java_Script/app.js
@@ -0,0 +1,23 @@
+let a = 5;
+let b = 12;
+
+if(a>b){
+    console.log(a);
+}else if (a<b) {
+    console.log(b);
+    
+} else {
+    console.log("a==b");
+}
+let size = "L";
+
+if(size == "XL"){
+    console.log(250);
+
+}else if(size = "L"){
+    console.log(200);
+}else if(size = "M"){
+    console.log(100);
+}else {
+    console.log(50);
+}
