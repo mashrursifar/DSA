a47b2efd3e887adcad210db31e7835a03ac5e3ec
mashrursifar <mashrursifar@gmail.com>
2026-07-08 17:45:27 +0600

    JS game

--- Developments/Java_Script/app.js
+++ Developments/Java_Script/app.js
@@ -1,23 +1,38 @@
-let a = 5;
-let b = 12;
+// let a = 5;
+// let b = 12;
 
-if(a>b){
-    console.log(a);
-}else if (a<b) {
-    console.log(b);
+// if(a>b){
+//     console.log(a);
+// }else if (a<b) {
+//     console.log(b);
     
-} else {
-    console.log("a==b");
-}
-let size = "L";
+// } else {
+//     console.log("a==b");
+// }
+// let size = "L";
 
-if(size == "XL"){
-    console.log(250);
+// if(size == "XL"){
+//     console.log(250);
 
-}else if(size = "L"){
-    console.log(200);
-}else if(size = "M"){
-    console.log(100);
-}else {
-    console.log(50);
+// }else if(size = "L"){
+//     console.log(200);
+// }else if(size = "M"){
+//     console.log(100);
+// }else {
+//     console.log(50);
+// }
+
+
+let movie = prompt("Guess my favourite movie name: ");;
+
+
+while ((movie != "john wick") && (movie !="quit")) {
+    movie= prompt("Wrong guess/n guess my favourite movie: ");
+
 }
+
+if(movie == "john wick"){
+        console.log("Correct guess");
+
+    }
+
