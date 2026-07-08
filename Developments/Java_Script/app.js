8179bab77475364d8f3262d48976f5cd753e50b0
mashrursifar <mashrursifar@gmail.com>
2026-07-08 20:34:47 +0600

    assignments

--- Developments/Java_Script/app.js
+++ Developments/Java_Script/app.js
@@ -36,3 +36,15 @@ if(movie == "john wick"){
 
     }
 
+
+let num = 2;
+let arr=[1,2,3,4,5,6,2,3];
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i]==num){
+        arr.splice(i,1);
+    }
+}
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
