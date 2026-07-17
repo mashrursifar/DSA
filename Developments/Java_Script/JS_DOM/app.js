448ae4b9ac7df98228d8445dfedd3f8c0ba1b766
mashrursifar <mashrursifar@gmail.com>
2026-07-17 22:15:33 +0600

    DOM add remove done

--- Developments/Java_Script/JS_DOM/app.js
+++ Developments/Java_Script/JS_DOM/app.js
@@ -1,5 +1,11 @@
-document.querySelector("#description");
+let p = document.createElement('p');
+p.innerText = "Hey there! I am appending ";
+p.classList.add('red');
+let body = document.querySelector('body');
+body.append(p);
 
-console.dir(document.querySelectorAll("p"));
 
-console.dir(document.querySelectorAll("div a"));
+let h3 = document.createElement('h3');
+h3.innerText = "I am h3 ";
+h3.classList.add('blue');
+body.append(h3);
