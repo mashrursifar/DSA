22bfd55a70fa37d262f0a2903c1d2b544e2c6d8e
mashrursifar <mashrursifar@gmail.com>
2026-07-21 22:56:59 +0600

    many settings

--- Developments/Java_Script/promises/app.js
+++ Developments/Java_Script/promises/app.js
@@ -1,1 +1,28 @@
-console.log("Starting Promise Practice");
+// Call back hell example
+
+// Suppose you need to store something in DB but
+// you need to save one after another. If first one
+// saved then second data. If second data saved then
+// third
+
+function saveToDB(data, success, failure) {
+  let mbps = Math.floor(Math.random() * 10) + 1;
+
+  if (mbps > 4) {
+    success();
+  } else {
+    failure();
+  }
+}
+
+saveToDB(
+  "Mashrur",
+  () => {
+    console.log("Success: Data is saved");
+  },
+  () => {
+    console.log("Failure: Data did not saved");
+  },
+);
+
+console.log();
