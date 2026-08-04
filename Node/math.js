88ec3e0a036a35a8e3fde5f4bba41a1603ae3ed6
mashrursifar <mashrursifar@gmail.com>
2026-08-04 21:16:03 +0600

    export requre

--- Node/math.js
+++ Node/math.js
@@ -3,4 +3,9 @@ const mul = (a,b)=>a*b;
 const g = 9.8;
 const PI = 3.1416;
 
-module.exports = 212;
+module.exports = {
+    sum : sum,
+    mul : mul,
+    g : g,
+    PI : PI
+};
--- Node/script.js
+++ Node/script.js
@@ -1,3 +1,4 @@
 const someVal = require("./math.js");
 
 console.log(someVal);
+console.log(someVal.mul(3,6));
