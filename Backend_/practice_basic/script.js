46f6d6e73877c279c21c064d9a805bbe2c1a32d3
mashrursifar <mashrursifar@gmail.com>
2026-08-05 21:11:55 +0600

    files

--- Backend_/practice_basic/script.js
+++ Backend_/practice_basic/script.js
@@ -4,7 +4,7 @@ console.log(someVal);
 console.log(someVal.mul(3, 6));
 
 //Imorting a directory
-const fruits = require("./newFile/index.js");
+const fruits = require("../newFile/index.js");
 
 console.log(fruits);
 console.log(fruits[0].banana.color);
