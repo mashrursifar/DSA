85b2015592b88c1f351800daf1c5c049e569d5dd
mashrursifar <mashrursifar@gmail.com>
2026-08-05 20:22:18 +0600

    Tab fixed

--- Backend_/Express/index.js
+++ Backend_/Express/index.js
@@ -4,24 +4,24 @@ const app = express();
 let port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server is started on ${port}`);
+    console.log(`Server is started on ${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>I am in the root<h1>");
-  console.log("You are in the root page!");
+    res.send("<h1>I am in the root<h1>");
+    console.log("You are in the root page!");
 });
 
 app.get("/search", (req, res) => {
-//   res.send("you are in the search page");
-  let { q } = req.query;
-  res.send(`You search ${q}`);
-  console.log(req.query);
-  console.log("You are in the searching page!");
+    //   res.send("you are in the search page");
+    let { q } = req.query;
+    res.send(`You search ${q}`);
+    console.log(req.query);
+    console.log("You are in the searching page!");
 });
 
 app.get("/:username/:pass", (req, res) => {
-  let { username, pass } = req.params;
-  console.log(req.params);
-  res.send(`User name is: ${username} and password ${pass}`);
+    let { username, pass } = req.params;
+    console.log(req.params);
+    res.send(`User name is: ${username} and password ${pass}`);
 });
