de56a1d7d86a455dfc96dd2f75de65c83706ae63
mashrursifar <mashrursifar@gmail.com>
2026-08-05 12:56:52 +0600

    get req and path parameter

---
+++ Backend_/Express/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+// console.dir(app);
+let port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is started on ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>I am in the root<h1>");
+  console.log("You are in the root page!");
+});
+
+app.get("/search", (req, res) => {
+  res.send("you are in the search page");
+  console.log("You are in the searching page!");
+});
+
+app.get("/:username/:pass", (req, res) => {
+    let {username,pass} = req.params
+    console.log(req.params);
+  res.send(`User name is: ${username} and password ${pass}`);
+});
