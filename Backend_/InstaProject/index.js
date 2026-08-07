f9bea635547a7c9ab8ed7122aad9dda891cba3cd
mashrursifar <mashrursifar@gmail.com>
2026-08-07 20:28:47 +0600

    basic data templating by ejs finished

--- Backend_/InstaProject/index.js
+++ Backend_/InstaProject/index.js
@@ -12,11 +12,14 @@ app.get("/", (req, res) => {
 });
 app.get("/accounts", (req, res) => {
     let { username } = req.query;
-    if(acc.cats.name[username]){
+    let data = acc[username]
+
+    if(data){
         console.log("Data found");
-        res.render("accounts.ejs", { username,followers: acc.username.name["followers"],following: acc.username.name["following"] });
+        res.render("accounts.ejs", { data });
     }else{
         console.log("Data didn't found");
+        res.render("notFound.ejs")
     }
 
     // console.log(acc);
