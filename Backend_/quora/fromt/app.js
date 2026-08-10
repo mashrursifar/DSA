e18c1f36bc4240c539d2af7e841ab0116bdb2294
mashrursifar <mashrursifar@gmail.com>
2026-08-10 21:54:39 +0600

    all

---
+++ Backend_/quora/fromt/app.js
@@ -0,0 +1,18 @@
+let like = document.querySelector(".like");
+let change = false;
+like.addEventListener("click", () => {
+    if(change){
+        like.classList.remove("likeTrue")
+        change = f;
+    }else{
+    like.classList.add("likeTrue");
+        change = true;
+    }
+    
+    
+});
+
+
+
+
+
