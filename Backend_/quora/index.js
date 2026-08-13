ea2db181161100d24f2967ef20562433aff7daf7
mashrursifar <mashrursifar@gmail.com>
2026-08-13 21:23:47 +0600

    Unique Id is added

--- Backend_/quora/index.js
+++ Backend_/quora/index.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 const port = 8080;
 
-app.set("views",path.join(__dirname,"/views"))
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
     console.log("Server started");
 });
 let posts = [
     {
+        id: uuid(),
         username: "Sifar",
         heading: "Ai is amazing",
         content: "Ai is changing the worls",
     },
     {
+        id: uuid(),
         username: "Rahim",
         heading: "JS",
         content: "I am learning JS",
@@ -34,8 +37,9 @@ app.get("/postCreate", (req, res) => {
 });
 
 app.post("/post", (req, res) => {
-    
+    let id = uuid();
     let { username, heading, content } = req.body;
-    posts.push({ username, heading, content });
-    res.render("post.ejs", {posts});
+    posts.push({ id, username, heading, content });
+    // res.render("post.ejs", { posts }); if we use it. then every refresh will add last post
+    res.redirect("/post");
 });
