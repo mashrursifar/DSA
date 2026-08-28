30a464b82e91bb48f8d70b05769490b881d408bb
mashrursifar <mashrursifar@gmail.com>
2026-08-28 23:23:46 +0600

    Mini

--- Backend_/miniWhatsapp/index.js
+++ Backend_/miniWhatsapp/index.js
@@ -7,6 +7,15 @@ const app = express()
 app.set("views",path.join(__dirname,"/views"))
 app.set("view engine","ejs")
 
+main().then(()=>{
+    console.log("Connected to DB");
+}).catch(err =>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp')
+}
 
 app.listen(8080, ()=>{
     console.log("Server started at port 8080");
